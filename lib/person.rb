6e4b86b8ef5a1b2645c59440164bd9f2544b8725
github username <scott.gloyna01@gmail.com>
2021-01-13 12:05:38 -0600

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,71 +1,67 @@
 # your code goes here
 require 'pry'
 class Person
-    attr_reader :name, :happiness
+    attr_reader :name, :happiness, :hygiene
     attr_accessor :bank_account
+    
     def initialize(name)
-    @name = name
-    @bank_account = 25
-    @happiness = 8
-    @hygiene = 8
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
     end
-    def happy_write(happy)
+
+    def happiness=(happy)
         @happiness = happy.clamp(0,10)
     end
-    def hygene
-        @hygiene = @hygiene.clamp(0,10)
+
+    def hygiene=(stinky)
+        @hygiene = stinky.clamp(0,10)
     end
 
     def clean?
         @hygiene > 7
     end
+
     def happy?
         @happiness > 7
     end
+
     def get_paid(salary)
         @bank_account += salary
         p "all about the benjamins"
     end
+
     def take_bath
-        @hygiene += 4
-        @hygiene = @hygiene.clamp(0, 10)
+        self.hygiene= self.hygiene + 4
         p "♪ Rub-a-dub just relaxing in the tub ♫"
     end
-    def call_friend(friend)
-        @happiness += 3
-        @happiness = @happiness.clamp(0, 10)
-        p "Hi #{friend.name}! It's #{self.name} How are you?"
-    end
-    def start_conversation(friend, topic)
-        if topic == "politics"
-            p "blah blah partisan blah lobbyist"
-            self.happy_write(self.happiness - 1)
-            friend.happy_write(friend.happiness - 1)
-            @happiness = @happiness.clamp(0,10)
-            #friend.happiness = friend.happiness.clamp(0,10)
 
+    def call_friend(friend)
+        self.happiness= self.happiness + 3
+        friend.happiness= friend.happiness + 3
+        p "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(friend, topic)
+
+        if topic == "politics"
+            self.happiness= self.happiness - 2
+            friend.happiness= friend.happiness - 2
+            p "blah blah partisan blah lobbyist"
         elsif topic == "weather"
-            binding.pry
-            happy_test = self.happiness + 1
-            binding.pry
+            self.happiness= self.happiness + 1
+            friend.happiness= friend.happiness + 1
             p "blah blah sun blah rain"
-            self.happy_write(happy_test)
-            binding.pry
-            friend.happy_write(friend.happiness + 1)
         else 
             p "blah blah blah blah blah"
         end
     end
+
     def work_out
-        @happiness += 2
-        @happiness = @happiness.clamp(0,10)
-        @hygiene -= 3
-        @hygiene = @hygiene.clamp(0,10)
+        self.happiness= self.happiness + 2
+        self.hygiene= self.hygiene - 3
         p "♪ another one bites the dust ♫"
     end
-    #def min_max(stat)
-     #stat.clamp(0,10)
-        
-    #end
 
 end
